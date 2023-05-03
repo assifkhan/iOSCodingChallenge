59460629bfb3ad7f7dbad842a9a50ffeea6a4d54
assifkhan <assifkhan@gmail.com>
2023-05-03 13:51:20 -0500

    Added table view
    make a list of dummy list in the tableview

--- iOSCodingChallenge/iOSCodingChallenge/ViewController.swift
+++ iOSCodingChallenge/iOSCodingChallenge/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView:UITableView!{
+        didSet{
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
+        
+        cell.textLabel?.text = "test"
+        
+        return cell
+    }
+}
